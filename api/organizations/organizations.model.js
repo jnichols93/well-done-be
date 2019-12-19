8d89156ddf2caefd98e055291ea530ad9b7c55dd
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2019-12-18 18:07:28 -0600

    update to model
    Pumps not showing up under associated accounts. writing queries to address this issue

--- api/organizations/organizations.model.js
+++ api/organizations/organizations.model.js
@@ -12,8 +12,20 @@ function findAllAndData() {
   return db("organizations as o")
     .join("accounts as a", "a.id", "o.org_id")
     .join("pumps as p", "o.id", "p.org_id")
-    .join("sensors as s", "o.id", "s.org_id")
-    .join("");
+    .join("sensors as s", "o.id", "s.org_id");
+}
+//get pumps by org id
+function getPumpsByOrgId(org_id){
+    return db("pumps as p")
+    .join("organizations as o", "o.id", "p.org_id")
+    .where("p.org_id", "=", "o.id");
+}
+
+//get users by org id
+function getUsersByOrg(org_id){
+  return db("accounts as a")
+  .join("organizations as o", "o.id","a.org_id")
+  .where("a.org_id","=", "o.id")
 }
 
 function findByOrgName(org_name) {
